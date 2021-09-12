4eade8b8097aa3c824a31a9c5f9c755157fdc009
djcrump03 <64335020+djcrump03@users.noreply.github.com>
2021-09-12 12:43:47 -0400

    setup reduxform

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Router, Switch, Route } from 'react-router-dom';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware()(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
 
 import './style/main.scss';
 
 import history from './history';
-
 import Layout from './components/layout';
 import Signin from './components/auth/signin';
 import Signup from './components/auth/signup';
-
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <Router history={history}>
         <Layout>
-            <Switch>
-              <Route path='/' exact component={Signin}/>
-            </Switch>
+          <Switch>
+            <Route path='/' exact component={Signin}/>
+            <Route path='/signin' exact component={Signin}/>
+            <Route path='/signup' exact component={Signup}/>
+          </Switch>
         </Layout>
       </Router>
     </Provider>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as form } from 'redux-form'; 
 
-const rootReducer = combineReducers({
-  state: (state = {}) => state
-});
 
-export default rootReducer;
+const rootReducer = combineReducers({	
+  form
+});	
+
+
+export default rootReducer; 
