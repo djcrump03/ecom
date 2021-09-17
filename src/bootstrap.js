339c54c4b64bf977558ee8d33e9f42394b7ac20f
djcrump03 <64335020+djcrump03@users.noreply.github.com>
2021-09-17 17:12:39 -0400

    built base structure for account components

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -4,15 +4,14 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Router, Switch, Route } from 'react-router-dom';
 import reducers from './reducers';
-
 const createStoreWithMiddleware = applyMiddleware()(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
-
 import './style/main.scss';
-
 import history from './history';
 import Layout from './components/layout';
 import Signin from './components/auth/signin';
 import Signup from './components/auth/signup';
+import Account from './components/account/account';
+
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
@@ -22,6 +21,8 @@ function main() {
             <Route path='/' exact component={Signin}/>
             <Route path='/signin' exact component={Signin}/>
             <Route path='/signup' exact component={Signup}/>
+
+            <Route path='/account' exact component={Account}/>
           </Switch>
         </Layout>
       </Router>
