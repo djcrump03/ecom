c998153a89407ab45d08c6ea7c3bf5a2d259d6fc
djcrump03 <64335020+djcrump03@users.noreply.github.com>
2021-09-17 19:13:54 -0400

    finished header feature

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
-
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
-
 import AccountInformation from './accountInformation';
 import PurchaseHistory from './purchaseHistory';
-
 class Account extends Component {
 
     componentDidMount() {
+
+        const headerLinks = [
+            {
+                _id: 0,
+                title: 'Shop',
+                path: '/shop'
+            },
+            {
+                _id: 1,
+                title: 'Logout',
+                path: '/'
+            }
+        ]
 
         const navbarLinks = [
             {
@@ -25,7 +35,7 @@ class Account extends Component {
             }
         ]
 
-        this.props.setHeaderLinks([]);
+        this.props.setHeaderLinks(headerLinks);
         this.props.setNavbarLinks(navbarLinks);
     }
 
@@ -40,7 +50,6 @@ class Account extends Component {
         }
         return jsx;
     }
-
     render() {
         return (
             <div>
@@ -49,12 +58,9 @@ class Account extends Component {
         )
     }
 }
-
 function mapStateToProps(state) {
     const { headerLinks, navbarLinks } = state.headerNavbar;
     return { headerLinks, navbarLinks }
 }
-
 Account = connect(mapStateToProps, actions)(Account);
-
 export default Account;
