f416f7733fc4c8f1ba4e9394b960171f55559de6
djcrump03 <64335020+djcrump03@users.noreply.github.com>
2021-09-17 21:42:23 -0400

    styled purchase detail

--- src/components/account/purchaseDetail.js
+++ src/components/account/purchaseDetail.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
 function PurchaseDetailLabel({className, title, value}) {
     return (
         <div className={`${className} purchase-detail-label`}>
@@ -9,17 +8,13 @@ function PurchaseDetailLabel({className, title, value}) {
         </div>
     )
 }
-
-
 // total: 19.40,
 // creditCard: '-0000',
-
-
 class PurchaseDetail extends Component {
     render() {
         const { className, orderNumber, orderDate, user, total, creditCard } = this.props;
         const { name, shippingAddress } = user;
-
+        const nameAddress = `${name}       ${shippingAddress}`
         return (
             <div className={`${className} purchase-detail`}>
                 <PurchaseDetailLabel
@@ -31,9 +26,9 @@ class PurchaseDetail extends Component {
                  title='Order Date'
                  value={orderDate}/>
                 <PurchaseDetailLabel
-                 className='purchase-detail__shipping'
+                 className='purchase-detail__shipping-address'
                  title='Shipping Address'
-                 value={`${name}\n${shippingAddress}`}/>
+                 value={nameAddress}/>
                 <PurchaseDetailLabel
                  className='purchase-detail__total'
                  title='Total'
@@ -42,18 +37,17 @@ class PurchaseDetail extends Component {
                  className='purchase-detail__credit-card'
                  title='Credit Card'
                  value={creditCard}/>
+                 <a className='purchase-detail__track-shipment'>Track Shipment</a>
+                 <a className='purchase-detail__print-receipt'>Print Receipt</a>
             </div>
         )
     }
 }
-
 function mapStateToProps(state) {
     const { purchaseDetail } = state.user;
     return {
         ...purchaseDetail
     }
 }
-
 PurchaseDetail = connect(mapStateToProps)(PurchaseDetail);
-
 export default PurchaseDetail;
