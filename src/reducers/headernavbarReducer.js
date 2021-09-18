267ba106399baa7f26ea55e153e5dd34be5e5620
djcrump03 <64335020+djcrump03@users.noreply.github.com>
2021-09-18 12:26:13 -0400

    refactored navbar to take additional onclick

--- src/reducers/headernavbarReducer.js
+++ src/reducers/headernavbarReducer.js
@@ -6,8 +6,10 @@ import {
 
 const INITIAL_STATE = {
     headerLinks: [],
-    navbarLinks: []
+    navbarLinks: [],
+    onClick: ''
 }
+
 export default function(state = INITIAL_STATE, action) {
     switch (action.type) {
         case SET_HEADER_LINKS:
@@ -16,9 +18,11 @@ export default function(state = INITIAL_STATE, action) {
                 headerLinks: action.payload
             }
         case SET_NAVBAR_LINKS:
+        const { links, onClick } = action.payload; 
             return {
                 ...state,
-                navbarLinks: action.payload
+                navbarLinks: links,
+                onClick: onClick
             }
         case CHANGE_NAVBAR_ACTIVE: 
             const navbarLinks = state.navbarLinks.map(link => {
