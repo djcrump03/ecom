e7467b3d0397f26ac831305ba47b80daa5020ed5
djcrump03 <64335020+djcrump03@users.noreply.github.com>
2021-09-28 21:36:17 -0500

    built shipping to component

--- src/components/information/paymentForm.js
+++ src/components/information/paymentForm.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 
 import { FormInput, FormButton } from '../formFields';
+import { UnderlinedTitle } from './infoHelp';
 
 import history from '../../history';
 import OrderSummary from './orderSummary';
@@ -54,6 +55,11 @@ class PaymentForm extends Component {
                 component={FormButton}/>
 
 <OrderSummary className='payment-form__order-summary'/>
+<div className='payment-form__shipping-info shipping-info'>
+                    <UnderlinedTitle className='shipping-info__title' title='Shipping To'/>
+                    <div className='shipping-info__name small-text'>Jordan Hudgens</div>
+                    <div className='shipping-info__address small-text'>1234 address goes here</div>
+                </div>
             </form>
         )
     }
